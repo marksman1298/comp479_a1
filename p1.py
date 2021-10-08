8a2c022853f1732c4ff38882c95e7f378f2a2d41
marksman1298 <marksaid206@gmail.com>
2021-10-08 11:26:12 -0400

    Can now select specific articles, some error handling

--- p1.py
+++ p1.py
@@ -3,9 +3,15 @@ from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
 
 
-def splitIntoArticles(dir, specificFile=""): #read all files in directory, put everything into one string, split into list of articles 
+def splitIntoArticles(dir): #read all files in directory, put everything into one string, split into list of articles 
     data = ""
-    listOfFiles = os.listdir(dir)
+    try:
+        dir = input("Enter the directory: ")
+        listOfFiles = os.listdir(dir)
+    except Exception as e: 
+        print(e)
+        return 
+    specificFile = input("Enter a specific file: ")
     if specificFile in listOfFiles and specificFile.endswith(".sgm"):
         filepath = dir + "/" + specificFile
         with open(filepath, "r") as f:
@@ -23,13 +29,26 @@ def splitIntoArticles(dir, specificFile=""): #read all files in directory, put e
 def makeDict(listOfArticles): #want to get all newids of articles, then add them to a list, then combine both lists into dictionary with key new id : value article
     articles = {}
     ids = []
+    specificArticle = input("Enter desired articles separated by space: ")
+    listArticles = specificArticle.split(" ")
     for i in range(len(listOfArticles)): 
         foundNewID = re.search('NEWID="([0-9]+)"', listOfArticles[i])
         if foundNewID is not None:
             newId = re.search('[0-9]+', foundNewID.group(0))
             if newId is not None:
                 ids.append(newId.group(0))
-    articles = {ids[i]: listOfArticles[i] for i in range(len(ids))}            
+    articles = {ids[i]: listOfArticles[i] for i in range(len(ids))}
+    
+    if not len(listArticles) == 0:
+        tableArticles = {} 
+        for articleId in listArticles:
+            try:
+                tableArticles[articleId] = articles[articleId]
+            except Exception as e:
+                print("Not a valid key", e)
+                return
+        return extractText(tableArticles)
+       
     return extractText(articles)
             
 def extractText(articles): #get all information between the text tags for each article
@@ -38,43 +57,52 @@ def extractText(articles): #get all information between the text tags for each a
         endText = articles[key].find("</TEXT>")
         if startText != -1 and endText != -1:
             articles[key] = articles[key][startText:endText+7]
-    with open("part1_reut2-004.txt", "w") as outfile:
+    with open("part1_article5.txt", "w") as outfile:
         json.dump(articles, outfile)
     return articles
      
-
-def tokenize(articles): #tokenize and inverse
-    for key in articles:
-        articles[key] = re.sub("<(.*?)>","" , articles[key])
-        articles[key] = re.sub("&#(.*?);", "", articles[key])
+def tokenize(articles): #tokenize 
+    if not articles:
+        print("Dictionary is empty")
+        return
+    for key in articles: 
         articles[key] = word_tokenize(articles[key])
-    with open("part2_reut2-004.txt", "w") as outfile:
+    with open("part2_article5.txt", "w") as outfile:
         json.dump(articles, outfile)
     return articles
 
 def toLowerCase(articles): #lower case all values in the strings
+    if not articles:
+        print("Dictionary is empty")
+        return
     for keys in articles:
         articles[keys]=list(map(str.lower, articles[keys]))
-    with open("part3_reut2-004.txt", "w") as outfile:
+    with open("part3_article5.txt", "w") as outfile:
         json.dump(articles, outfile)
     return articles
 
 def porterStemmer(articles):
+    if not articles:
+        print("Dictionary is empty")
+        return
     ps = PorterStemmer()
     for keys in articles:
         articles[keys] = list(map(ps.stem, articles[keys]))
-    with open("part4_reut2-004.txt", "w") as outfile:
+    with open("part4_article5.txt", "w") as outfile:
         json.dump(articles, outfile)
     return articles
 
 def removeStopWords(articles):
+    if not articles:
+        print("Dictionary is empty")
+        return
     words = input("Enter stop words separated by spaces: ")
     listWords = words.split(" ")
     for keys in articles:
         for token in reversed(articles[keys]):
             if token in listWords:
                 articles[keys].remove(token)
-    with open("part5_reut2-004.txt", "w") as outfile:
+    with open("part5_article5.txt", "w") as outfile:
         json.dump(articles, outfile)
     return articles
     
@@ -82,9 +110,8 @@ def removeStopWords(articles):
 
 
 def pipeline():
-    dir = input("Enter the directory: ")
     articles = [] 
-    articles = splitIntoArticles(dir, "reut2-004.sgm")
+    articles = splitIntoArticles(dir) 
     articles = tokenize(articles)
     articles = toLowerCase(articles)
     articles = porterStemmer(articles)
